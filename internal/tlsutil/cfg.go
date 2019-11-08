65c2e5f0fab27b59f6e20e779daaa01f82e302fa
Matthew Fisher <matt.fisher@microsoft.com>
2019-11-08 07:39:57 -0800

    ref(tlsutil): remove ServerConfig
    dead code from Tiller days
    
    Signed-off-by: Matthew Fisher <matt.fisher@microsoft.com>

--- internal/tlsutil/cfg.go
+++ internal/tlsutil/cfg.go
@@ -27,9 +27,7 @@ import (
 // Options represents configurable options used to create client and server TLS configurations.
 type Options struct {
 	CaCertFile string
-	// If either the KeyFile or CertFile is empty, ClientConfig() will not load them,
-	// preventing Helm from authenticating to Tiller. They are required to be non-empty
-	// when calling ServerConfig, otherwise an error is returned.
+	// If either the KeyFile or CertFile is empty, ClientConfig() will not load them.
 	KeyFile  string
 	CertFile string
 	// Client-only options
@@ -65,26 +63,5 @@ func ClientConfig(opts Options) (cfg *tls.Config, err error) {
 		ServerName:         opts.ServerName,
 		RootCAs:            pool,
 	}
-	return cfg, nil
-}
-
-// ServerConfig returns a TLS configuration for use by the Tiller server.
-func ServerConfig(opts Options) (cfg *tls.Config, err error) {
-	var cert *tls.Certificate
-	var pool *x509.CertPool
-
-	if cert, err = CertFromFilePair(opts.CertFile, opts.KeyFile); err != nil {
-		if os.IsNotExist(err) {
-			return nil, errors.Wrapf(err, "could not load x509 key pair (cert: %q, key: %q)", opts.CertFile, opts.KeyFile)
-		}
-		return nil, errors.Wrapf(err, "could not read x509 key pair (cert: %q, key: %q)", opts.CertFile, opts.KeyFile)
-	}
-	if opts.ClientAuth >= tls.VerifyClientCertIfGiven && opts.CaCertFile != "" {
-		if pool, err = CertPoolFromFile(opts.CaCertFile); err != nil {
-			return nil, err
-		}
-	}
-
-	cfg = &tls.Config{MinVersion: tls.VersionTLS12, ClientAuth: opts.ClientAuth, Certificates: []tls.Certificate{*cert}, ClientCAs: pool}
 	return cfg, nil
 }
